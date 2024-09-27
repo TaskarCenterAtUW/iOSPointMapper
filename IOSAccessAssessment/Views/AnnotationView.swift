58220aa5caa124cd3bda566e58e01fe174a1f62e
himanshunaidu <himanshunaidu12161997@gmail.com>
2024-09-26 23:16:35 -0700

    Move AnnotationCameraViewController to separate camera file

--- IOSAccessAssessment/Views/AnnotationView.swift
+++ IOSAccessAssessment/Views/AnnotationView.swift
@@ -104,67 +104,6 @@ struct AnnotationView: View {
     }
 }
 
-
-class AnnotationCameraViewController: UIViewController {
-    var cameraImage: UIImage?
-    var segmentationImage: CIImage?
-    var cameraView: UIImageView? = nil
-    var segmentationView: UIImageView? = nil
-    var sharedImageData: SharedImageData?
-    
-    init(sharedImageData: SharedImageData, index: Int) {
-        self.cameraImage = sharedImageData.cameraImage
-        self.segmentationImage = sharedImageData.classImages[index]
-//        self.cameraImage = cameraImage
-//        self.segmentationImage = segmentationImage
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-//        print("view.bounds.width: \(view.bounds.width)")
-//        print("main.bounds.width: \(UIScreen.main.bounds.width)")
-        let centerX = (UIScreen.main.bounds.width - 256.0) / 2.0
-        cameraView = UIImageView(image: cameraImage)
-        cameraView?.frame = CGRect(x: centerX, y: 0.0, width: 256.0, height: 256.0)
-        cameraView?.contentMode = .scaleAspectFill
-        if let cameraView = cameraView {
-            view.addSubview(cameraView)
-        }
-        
-        segmentationView = UIImageView(image: UIImage(ciImage: segmentationImage!, scale: 1.0, orientation: .downMirrored))
-        segmentationView?.frame = CGRect(x: centerX, y: 0.0, width: 256.0, height: 256.0)
-        segmentationView?.contentMode = .scaleAspectFill
-        if let segmentationView = segmentationView {
-            view.addSubview(segmentationView)
-        }
-        cameraView?.bringSubviewToFront(segmentationView!)
-    }
-}
-
-struct HostedAnnotationCameraViewController: UIViewControllerRepresentable{
-//    var cameraImage: UIImage
-//    var segmentationImage: UIImage
-    let sharedImageData: SharedImageData
-    let index: Int
-    
-    func makeUIViewController(context: Context) -> AnnotationCameraViewController {
-        return AnnotationCameraViewController(sharedImageData: sharedImageData, index: index)
-    }
-    
-    func updateUIViewController(_ uiViewController: AnnotationCameraViewController, context: Context) {
-        uiViewController.cameraImage = sharedImageData.cameraImage
-        uiViewController.segmentationImage = sharedImageData.classImages[index]
-//        uiViewController.cameraImage = cameraImage
-//        uiViewController.segmentationImage = segmentationImage
-        uiViewController.viewDidLoad()
-    }
-}
-
 //struct AnnotationView: View {
 //    @State private var index = 0
 //    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
