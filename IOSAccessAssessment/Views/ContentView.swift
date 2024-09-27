929f2c5e3961170985fe8460953775b37ade69d7
himanshunaidu <himanshunaidu12161997@gmail.com>
2024-09-26 22:33:24 -0700

    Create new folder Camera with dedicated Camera related functionality

--- IOSAccessAssessment/Views/ContentView.swift
+++ IOSAccessAssessment/Views/ContentView.swift
@@ -105,59 +105,6 @@ struct ContentView: View {
     }
 }
 
-class CameraViewController: UIViewController {
-    var session: AVCaptureSession?
-    var rootLayer: CALayer! = nil
-    private var previewLayer: AVCaptureVideoPreviewLayer! = nil
-//    var detectionLayer: CALayer! = nil
-//    var detectionView: UIImageView! = nil
-    
-    init(session: AVCaptureSession) {
-        self.session = session
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setUp(session: session!)
-    }
-    
-    private func setUp(session: AVCaptureSession) {
-        previewLayer = AVCaptureVideoPreviewLayer(session: session)
-        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        previewLayer.frame = CGRect(x: 0.0, y: 0.0, width: 256.0, height: 256.0)
-//        previewLayer.borderWidth = 2.0
-//        previewLayer.borderColor = UIColor.blue.cgColor
-//
-//        detectionView = UIImageView()
-//        detectionView.frame = CGRect(x: 59, y: 366, width: 280, height: 280)
-//        detectionView.transform = CGAffineTransform(rotationAngle: -.pi / 2)
-//        detectionView.layer.borderWidth = 2.0
-//        detectionView.layer.borderColor = UIColor.blue.cgColor
-        
-        DispatchQueue.main.async { [weak self] in
-            self!.view.layer.addSublayer(self!.previewLayer)
-            //self!.view.layer.addSublayer(self!.detectionLayer)
-        }
-    }
-}
-
-struct HostedCameraViewController: UIViewControllerRepresentable{
-    var session: AVCaptureSession!
-    
-    func makeUIViewController(context: Context) -> CameraViewController {
-        annotationView = false
-        return CameraViewController(session: session)
-    }
-    
-    func updateUIViewController(_ uiView: CameraViewController, context: Context) {
-    }
-}
-
 class SegmentationViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     var segmentationView: UIImageView! = nil
     var sharedImageData: SharedImageData?
