9e9b6bcb2922bcc6f4439f3f9f1f1a2db0640a2b
himanshunaidu <himanshunaidu12161997@gmail.com>
2024-10-10 14:17:27 -0700

    Create ImageSaver file

---
+++ IOSAccessAssessment/ImageProcessing/ImageSaver.swift
@@ -0,0 +1,38 @@
+//
+//  ImageSaver.swift
+//  IOSAccessAssessment
+//
+//  Created by TCAT on 10/10/24.
+//
+
+import SwiftUI
+
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+    
+    func writeDepthMapToPhotoAlbum(cvPixelBufferDepth: CVPixelBuffer) {
+        // CVPixelBuffer to UIImage
+        let ciImageDepth            = CIImage(cvPixelBuffer: cvPixelBufferDepth)
+        let contextDepth:CIContext  = CIContext.init(options: nil)
+        let cgImageDepth:CGImage    = contextDepth.createCGImage(ciImageDepth, from: ciImageDepth.extent)!
+        let uiImageDepth:UIImage    = UIImage(cgImage: cgImageDepth, scale: 1, orientation: UIImage.Orientation.up)
+
+        // Save UIImage to Photos Album
+        UIImageWriteToSavedPhotosAlbum(uiImageDepth, self, #selector(saveCompleted), nil)
+    }
+    
+    func writeCIImageToPhotoAlbum(ciImage: CIImage) {
+        let contextDepth:CIContext  = CIContext.init(options: nil)
+        let cgImageDepth:CGImage    = contextDepth.createCGImage(ciImage, from: ciImage.extent)!
+        let uiImageDepth:UIImage    = UIImage(cgImage: cgImageDepth, scale: 1, orientation: UIImage.Orientation.up)
+
+        // Save UIImage to Photos Album
+        UIImageWriteToSavedPhotosAlbum(uiImageDepth, self, #selector(saveCompleted), nil)
+    }
+
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished!")
+    }
+}
