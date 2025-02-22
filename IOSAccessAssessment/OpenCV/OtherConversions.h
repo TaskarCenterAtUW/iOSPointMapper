404783ff78032639f6caa8610e11182ed3c9489a
himanshunaidu <himanshunaidu12161997@gmail.com>
2025-02-22 15:13:13 -0800

    Additional debugging on OtherConversions

--- IOSAccessAssessment/OpenCV/OtherConversions.h
+++ IOSAccessAssessment/OpenCV/OtherConversions.h
@@ -6,16 +6,9 @@
 //
 
 #ifdef __cplusplus
-#undef NO
-#undef YES
-
-/*
- Importing only the needed features can mitigate against build issues with other parts of openCV
- FIXME: These are the wrong imports
- */
-#import <opencv2/stitching.hpp>
-#import <opencv2/imgcodecs.hpp>
-
+#import <vector>
+#import <opencv2/core.hpp>       // Core structures (cv::Mat, cv::Point, cv::Vec3b)
+#import <opencv2/imgproc.hpp>    // Image processing (contours, morphology, etc.)
 #endif
 
 #import <UIKit/UIKit.h>
@@ -24,10 +17,14 @@
 
 + (NSArray<NSArray<NSValue *> *> *)cvContoursToNSArray:(const std::vector<std::vector<cv::Point>> &)contours;
 
++ (std::vector<cv::Point>)convertNSArrayToCvPoints:(NSArray<NSArray<NSValue *> *> *)points;
+
 /**
     Convert cv::Vec3b to NSArray<NSValue *>.
     FIXME: This method is not generic and should be refactored.
  */
 + (NSArray<NSValue *> *)convertVec3bArray:(const std::vector<cv::Vec3b> &)vecArray;
+
++ (std::vector<cv::Vec3b>)convertNSArrayToVec3bArray:(NSArray<NSValue *> *)array;
 
 @end
--- IOSAccessAssessment/OpenCV/OtherConversions.mm
+++ IOSAccessAssessment/OpenCV/OtherConversions.mm
@@ -6,6 +6,7 @@
 //
 
 #include "OtherConversions.h"
+#import <iostream>
 
 @implementation OtherConversions
 
@@ -24,6 +25,20 @@
     return convertedContours;
 }
 
++ (std::vector<cv::Point>)convertNSArrayToCvPoints:(NSArray<NSArray<NSValue *> *> *)points {
+    std::vector<cv::Point> convertedPoints;
+    
+    for (NSArray<NSValue *> *pointArray in points) {
+        for (NSValue *pointValue in pointArray) {
+            CGPoint cgPoint = [pointValue CGPointValue];
+            cv::Point point = cv::Point(cgPoint.x, cgPoint.y);
+            convertedPoints.push_back(point);
+        }
+    }
+    return convertedPoints;
+}
+
+
 + (NSArray<NSValue *> *)convertVec3bArray:(const std::vector<cv::Vec3b> &)vecArray {
     NSMutableArray<NSValue *> *convertedVec3bArray = [NSMutableArray array];
     
@@ -32,6 +47,20 @@
         uint8_t values[3] = {vec[0], vec[1], vec[2]};
         [convertedVec3bArray addObject:[NSValue valueWithBytes:&values objCType:@encode(uint8_t[3])]];
     }
+    return convertedVec3bArray;
+}
+
++ (std::vector<cv::Vec3b>)convertNSArrayToVec3bArray:(NSArray<NSValue *> *)array {
+    std::vector<cv::Vec3b> convertedVec3bArray;
+    
+    for (NSValue *value in array) {
+        uint8_t values[3];
+        [value getValue:&values];
+        
+        cv::Vec3b vec(values[0], values[1], values[2]);
+        convertedVec3bArray.push_back(vec);
+    }
+    
     return convertedVec3bArray;
 }
 
