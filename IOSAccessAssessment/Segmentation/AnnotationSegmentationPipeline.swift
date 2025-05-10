6cce6884fc4b49048a081a329571687d294738c6
himanshunaidu <himanshunaidu12161997@gmail.com>
2025-05-10 14:12:02 -0700

    Complete first version of AnnotationSegmentationPipeline that performs iterative homography transformation

--- IOSAccessAssessment/Segmentation/AnnotationSegmentationPipeline.swift
+++ IOSAccessAssessment/Segmentation/AnnotationSegmentationPipeline.swift
@@ -14,9 +14,11 @@ import simd
 
 /**
  A class to handle the segmentation pipeline for annotation purposes.
- Unlike the main SegmentationPipeline, this class processes images synchronously.
+ Unlike the main SegmentationPipeline, this class processes images and returns the results synchronously.
+ Later, we can consider making it asynchronous based on the requirements.
  */
 class AnnotationSegmentationPipeline {
+    // This will be useful only when we are using the pipeline in asynchronous mode.
     var isProcessing = false
     
     var selectionClasses: [Int] = []
@@ -29,4 +31,74 @@ class AnnotationSegmentationPipeline {
     // TODO: Check what would be the appropriate value for this
     // For normalized points
     var perimeterThreshold: Float = 0.01
+    
+    var contourRequestProcessor: ContourRequestProcessor?
+    var homographyTransformFilter: HomographyTransformFilter?
+    
+    init() {
+        self.contourRequestProcessor = ContourRequestProcessor(
+            contourEpsilon: self.contourEpsilon,
+            perimeterThreshold: self.perimeterThreshold,
+            selectionClassLabels: self.selectionClassLabels)
+        self.homographyTransformFilter = HomographyTransformFilter()
+    }
+    
+    func reset() {
+        self.isProcessing = false
+        self.setSelectionClasses([])
+    }
+    
+    func setSelectionClasses(_ selectionClasses: [Int]) {
+        self.selectionClasses = selectionClasses
+        self.selectionClassLabels = selectionClasses.map { Constants.ClassConstants.labels[$0] }
+        self.selectionClassGrayscaleValues = selectionClasses.map { Constants.ClassConstants.grayscaleValues[$0] }
+        self.selectionClassColors = selectionClasses.map { Constants.ClassConstants.colors[$0] }
+        
+        self.contourRequestProcessor?.setSelectionClassLabels(self.selectionClassLabels)
+    }
+    
+    func processRequest(imageDataHistory: [ImageData]) -> [CIImage]? {
+        if self.isProcessing {
+            print("Already processing a request.")
+            return nil
+        }
+        guard let homographyTransformFilter = self.homographyTransformFilter else {
+            print("Homography transform filter is not initialized.")
+            return nil
+        }
+        
+        self.isProcessing = true
+        
+        if imageDataHistory.count <= 1 {
+            print("No image history available for processing.")
+            self.isProcessing = false
+            if imageDataHistory.count == 1 && imageDataHistory[0].segmentationLabelImage != nil {
+                return [imageDataHistory[0].segmentationLabelImage!]
+            }
+            return nil
+        }
+        var transformedSegmentationLabelImages: [CIImage] = []
+        
+        /**
+         Iterate through the image data history in reverse.
+         Process each image by applying the homography transforms of the successive images.
+         */
+        // Identity matrix for the first image
+        var transformMatrix: simd_float3x3 = matrix_identity_float3x3
+        for i in (0..<imageDataHistory.count-1).reversed() {
+            let currentImageData = imageDataHistory[i]
+            let nextImageData = imageDataHistory[i+1]
+            transformMatrix = (nextImageData.transformMatrixToNextFrame?.inverse ?? matrix_identity_float3x3) * transformMatrix
+            // Apply the homography transform to the current image
+            var transformedImage = homographyTransformFilter.apply(
+                to: currentImageData.segmentationLabelImage!, transformMatrix: transformMatrix)
+            if let transformedSegmentationLabelImage = transformedImage {
+                transformedSegmentationLabelImages.append(transformedSegmentationLabelImage)
+            } else {
+                print("Failed to apply homography transform to the image.")
+            }
+        }
+        
+        return transformedSegmentationLabelImages
+    }
 }
