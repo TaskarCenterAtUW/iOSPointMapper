4de65ac47924d40424a391283ef7a28d415fa1c8
himanshunaidu <himanshunaidu12161997@gmail.com>
2025-05-12 13:41:11 -0700

    Add configurable parameterization to contour object rasterizer

--- IOSAccessAssessment/Segmentation/Helpers/ContourObjectRasterizer.swift
+++ IOSAccessAssessment/Segmentation/Helpers/ContourObjectRasterizer.swift
@@ -12,77 +12,105 @@ import UIKit
     Rasterizes detected contour objects into a CIImage.
     A helper function that is not used in the main app currently, but can be useful for debugging or visualization purposes.
  */
-func rasterizeContourObjects(objects: [DetectedObject], size: CGSize) -> CIImage? {
-    func colorForClass(_ classLabel: UInt8, labelToColorMap: [UInt8: CIColor]) -> UIColor {
-        let color = labelToColorMap[classLabel] ?? CIColor(red: 0, green: 0, blue: 0)
-        return UIColor(red: color.red, green: color.green, blue: color.blue, alpha: 1.0)
-    }
+
+// Temporary config struct
+struct RasterizeConfig {
+    let draw: Bool
+    let color: UIColor
+    let width: CGFloat
     
-    func createPath(points: [SIMD2<Float>], size: CGSize) -> UIBezierPath {
-        let path = UIBezierPath()
-        guard let firstPoint = points.first else { return path }
-        
-        let firstPixelPoint = CGPoint(x: CGFloat(firstPoint.x) * size.width, y: (1 - CGFloat(firstPoint.y)) * size.height)
-        path.move(to: firstPixelPoint)
-        
-        for point in points.dropFirst() {
-            let pixelPoint = CGPoint(x: CGFloat(point.x) * size.width, y: (1 - CGFloat(point.y)) * size.height)
-            path.addLine(to: pixelPoint)
+    init(draw: Bool = true, color: UIColor = .white, width: CGFloat = 2.0) {
+        self.draw = draw
+        self.color = color
+        self.width = width
+    }
+}
+
+struct ContourObjectRasterizer {
+    static func rasterizeContourObjects(
+        objects: [DetectedObject], size: CGSize,
+        polygonConfig: RasterizeConfig = RasterizeConfig(color: .green, width: 2.0),
+        boundsConfig: RasterizeConfig = RasterizeConfig(color: .red, width: 2.0),
+        wayBoundsConfig: RasterizeConfig = RasterizeConfig(color: .blue, width: 2.0),
+        centroidConfig: RasterizeConfig = RasterizeConfig(color: .white, width: 2.0)
+    ) -> CIImage? {
+        func colorForClass(_ classLabel: UInt8, labelToColorMap: [UInt8: CIColor]) -> UIColor {
+            let color = labelToColorMap[classLabel] ?? CIColor(red: 0, green: 0, blue: 0)
+            return UIColor(red: color.red, green: color.green, blue: color.blue, alpha: 1.0)
         }
-        path.close()
-        return path
-    }
-    
-    UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
-    guard let context = UIGraphicsGetCurrentContext() else { return nil }
-    
-    let labelToColorMap = Constants.ClassConstants.labelToColorMap
-    for object in objects {
-        let color = colorForClass(object.classLabel, labelToColorMap: labelToColorMap)
         
-        /// First, draw the contour
-        let path = createPath(points: object.normalizedPoints, size: size)
-        
-//        context.setFillColor(color.cgColor)
-        context.setStrokeColor(color.cgColor)
-        context.setLineWidth(4.0)
-        context.addPath(path.cgPath)
-//        context.fillPath()
-        context.strokePath()
-        
-        /// Then, draw the way bound if exists, else draw the bounding box
-        if object.wayBounds != nil {
-            let wayBounds = object.wayBounds!
-            let wayPath = createPath(points: wayBounds, size: size)
-            context.setStrokeColor(color.cgColor)
-            context.setLineWidth(2.0)
-            context.addPath(wayPath.cgPath)
-            context.strokePath()
-        } else {
-            let boundingBox = object.boundingBox
-            let boundingBoxRect = CGRect(x: CGFloat(boundingBox.origin.x) * size.width,
-                                            y: (1 - CGFloat(boundingBox.origin.y + boundingBox.size.height)) * size.height,
-                                            width: CGFloat(boundingBox.size.width) * size.width,
-                                            height: CGFloat(boundingBox.size.height) * size.height)
+        func createPath(points: [SIMD2<Float>], size: CGSize) -> UIBezierPath {
+            let path = UIBezierPath()
+            guard let firstPoint = points.first else { return path }
             
-            context.setStrokeColor(color.cgColor)
-            context.setLineWidth(2.0)
-            context.addRect(boundingBoxRect)
-            context.strokePath()
+            let firstPixelPoint = CGPoint(x: CGFloat(firstPoint.x) * size.width, y: (1 - CGFloat(firstPoint.y)) * size.height)
+            path.move(to: firstPixelPoint)
+            
+            for point in points.dropFirst() {
+                let pixelPoint = CGPoint(x: CGFloat(point.x) * size.width, y: (1 - CGFloat(point.y)) * size.height)
+                path.addLine(to: pixelPoint)
+            }
+            path.close()
+            return path
         }
         
-        /// Lastly, circle the center point
-        let centroid = object.centroid
-        let centroidPoint = CGPoint(x: CGFloat(centroid.x) * size.width, y: (1 - CGFloat(centroid.y)) * size.height)
-        context.setFillColor(color.cgColor)
-        context.addEllipse(in: CGRect(x: centroidPoint.x - 5, y: centroidPoint.y - 5, width: 10, height: 10))
-        context.fillPath()
+        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        
+        let labelToColorMap = Constants.ClassConstants.labelToColorMap
+        for object in objects {
+            /// First, draw the contour
+            if polygonConfig.draw {
+                let path = createPath(points: object.normalizedPoints, size: size)
+                
+                context.setStrokeColor(polygonConfig.color.cgColor)
+                context.setLineWidth(polygonConfig.width)
+                context.addPath(path.cgPath)
+        //        context.fillPath()
+                context.strokePath()
+            }
+            
+            /// Then, draw the way bound if exists
+            if wayBoundsConfig.draw && object.wayBounds != nil {
+                let wayBounds = object.wayBounds!
+                let wayPath = createPath(points: wayBounds, size: size)
+                context.setStrokeColor(wayBoundsConfig.color.cgColor)
+                context.setLineWidth(wayBoundsConfig.width)
+                context.addPath(wayPath.cgPath)
+                context.strokePath()
+            }
+            
+            if boundsConfig.draw {
+                let boundingBox = object.boundingBox
+                let boundingBoxRect = CGRect(x: CGFloat(boundingBox.origin.x) * size.width,
+                                                y: (1 - CGFloat(boundingBox.origin.y + boundingBox.size.height)) * size.height,
+                                                width: CGFloat(boundingBox.size.width) * size.width,
+                                                height: CGFloat(boundingBox.size.height) * size.height)
+                
+                context.setStrokeColor(boundsConfig.color.cgColor)
+                context.setLineWidth(boundsConfig.width)
+                context.addRect(boundingBoxRect)
+                context.strokePath()
+            }
+            
+            /// Lastly, circle the center point
+            if centroidConfig.draw {
+                let centroid = object.centroid
+                let centroidPoint = CGPoint(x: CGFloat(centroid.x) * size.width, y: (1 - CGFloat(centroid.y)) * size.height)
+                context.setFillColor(centroidConfig.color.cgColor)
+                context.addEllipse(in: CGRect(x: centroidPoint.x - centroidConfig.width,
+                                              y: centroidPoint.y - centroidConfig.width,
+                                              width: 2 * centroidConfig.width,
+                                              height: 2 * centroidConfig.width))
+                context.fillPath()
+            }
+        }
+        let cgImage = UIGraphicsGetImageFromCurrentImageContext()?.cgImage
+        UIGraphicsEndImageContext()
+        
+        if let cgImage = cgImage {
+            return CIImage(cgImage: cgImage)
+        }
+        return nil
     }
-    let cgImage = UIGraphicsGetImageFromCurrentImageContext()?.cgImage
-    UIGraphicsEndImageContext()
-    
-    if let cgImage = cgImage {
-        return CIImage(cgImage: cgImage)
-    }
-    return nil
 }
