b10b301c5c214508a04ae72cfccadf3b47846465
himanshunaidu <himanshunaidu12161997@gmail.com>
2025-05-18 16:51:36 -0700

    Add OrientationObserver

---
+++ IOSAccessAssessment/Camera/OrientationObserver.swift
@@ -0,0 +1,24 @@
+//
+//  OrientationObserver.swift
+//  IOSAccessAssessment
+//
+//  Created by Himanshu on 5/18/25.
+//
+
+import Combine
+
+final class OrientationObserver: ObservableObject {
+    @Published var deviceOrientation: UIDeviceOrientation = UIDevice.current.orientation
+    private var cancellables = Set<AnyCancellable>()
+
+    init() {
+        NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)
+            .sink { [weak self] _ in
+                let orientation = UIDevice.current.orientation
+                if orientation.isValidInterfaceOrientation {
+                    self?.deviceOrientation = orientation
+                }
+            }
+            .store(in: &cancellables)
+    }
+}
