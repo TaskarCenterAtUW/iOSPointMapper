303a345ca0178a5e3b794abb1a09b9db5940d9d5
himanshunaidu <himanshunaidu12161997@gmail.com>
2025-05-18 19:25:01 -0700

    Complete basic functionality of AnnotationImageManager

--- IOSAccessAssessment/Annotation/AnnotationImageManager.swift
+++ IOSAccessAssessment/Annotation/AnnotationImageManager.swift
@@ -10,6 +10,10 @@ import SwiftUI
 import Combine
 import simd
 
+typealias AnnotationCameraUIImageOutput = (cgImage: CGImage, uiImage: UIImage)
+typealias AnnotationSegmentationUIImageOutput = (ciImage: CIImage?, uiImage: UIImage)
+typealias AnnotationObjectsUIImageOutput = (annotatedDetectedObjects: [AnnotatedDetectedObject], selectedObjectId: UUID, uiImage: UIImage)
+
 class AnnotationImageManager: ObservableObject {
     @Published var cameraUIImage: UIImage? = nil
     @Published var segmentationUIImage: UIImage? = nil
@@ -18,8 +22,11 @@ class AnnotationImageManager: ObservableObject {
     @State private var transformedLabelImages: [CIImage]? = nil
     
     @Published var annotatedSegmentationLabelImage: CIImage? = nil
+    @Published var selectedObjectId: UUID? = nil
     @Published var annotatedDetectedObjects: [AnnotatedDetectedObject]? = nil
     
+    // Helpers
+    private let annotationCIContext = CIContext()
     private let annotationSegmentationPipeline = AnnotationSegmentationPipeline()
     private let grayscaleToColorMasker = GrayscaleToColorCIFilter()
     
@@ -28,7 +35,7 @@ class AnnotationImageManager: ObservableObject {
         
     }
     
-    func update(currentImage: CIImage, imageHistory: [ImageData]) {
+    func update(segmentationLabelImage: CIImage, imageHistory: [ImageData]) {
         let transformedLabelImages = transformImageHistoryForUnionOfMasks(imageDataHistory: imageHistory)
     }
     
@@ -42,5 +49,125 @@ class AnnotationImageManager: ObservableObject {
             print("Error processing transformations request: \(error)")
         }
         return nil
+    }
+    
+    private func setCameraUIImage(cameraImage: CIImage) -> AnnotationCameraUIImageOutput {
+        let cameraCGImage = annotationCIContext.createCGImage(
+            cameraImage, from: cameraImage.extent)!
+        let cameraUIImage = UIImage(cgImage: cameraCGImage, scale: 1.0, orientation: .up)
+        return (cgImage: cameraCGImage,
+                uiImage: cameraUIImage)
+    }
+    
+    // Perform the union of masks on the label image history for the given segmentation class.
+    // Save the resultant image to the segmentedLabelImage property.
+    private func setAndReturnSegmentationUIImage(segmentationLabelImage: CIImage, segmentationClass: SegmentationClass)
+    -> AnnotationSegmentationUIImageOutput {
+        var inputLabelImage = segmentationLabelImage
+        do {
+            inputLabelImage = try self.annotationSegmentationPipeline.processUnionOfMasksRequest(
+                targetValue: segmentationClass.labelValue,
+                bounds: segmentationClass.bounds
+            )
+        } catch {
+            print("Error processing union of masks request: \(error)")
+        }
+//        self.annotatedSegmentationLabelImage = inputLabelImage
+        self.grayscaleToColorMasker.inputImage = inputLabelImage
+        self.grayscaleToColorMasker.grayscaleValues = [segmentationClass.grayscaleValue]
+        self.grayscaleToColorMasker.colorValues = [segmentationClass.color]
+        
+        let segmentationUIImage = UIImage(ciImage: self.grayscaleToColorMasker.outputImage!, scale: 1.0, orientation: .up)
+        return (ciImage: inputLabelImage,
+                uiImage: segmentationUIImage)
+    }
+    
+    private func setAndReturnObjectsUIImage(inputLabelImage: CIImage, segmentationClass: SegmentationClass)
+    -> AnnotationObjectsUIImageOutput {
+        var inputDetectedObjects: [DetectedObject] = []
+        
+        // Get the detected objects from the resultant union image.
+        do {
+            inputDetectedObjects = try self.annotationSegmentationPipeline.processContourRequest(
+                from: inputLabelImage,
+                targetValue: segmentationClass.labelValue,
+                isWay: segmentationClass.isWay,
+                bounds: segmentationClass.bounds
+            )
+        } catch {
+            print("Error processing contour request: \(error)")
+        }
+        var annotatedDetectedObjects = inputDetectedObjects.enumerated().map({ objectIndex, object in
+            AnnotatedDetectedObject(object: object, classLabel: object.classLabel, depthValue: 0.0, isAll: false,
+                                    label: segmentationClass.name + ": " + String(objectIndex))
+        })
+        // Add the "all" object to the beginning of the list
+        annotatedDetectedObjects.insert(
+            AnnotatedDetectedObject(object: nil, classLabel: segmentationClass.labelValue,
+                                    depthValue: 0.0, isAll: true, label: AnnotationViewConstants.Texts.selectAllLabelText),
+            at: 0
+        )
+//        self.annotatedDetectedObjects = annotatedDetectedObjects
+        let selectedObjectId = annotatedDetectedObjects[0].id
+        let objectsUIImage = UIImage(
+            cgImage: ContourObjectRasterizer.rasterizeContourObjects(
+                objects: inputDetectedObjects,
+                size: Constants.ClassConstants.inputSize,
+                polygonConfig: RasterizeConfig(draw: true, color: nil, width: 2),
+                boundsConfig: RasterizeConfig(draw: false, color: nil, width: 0),
+                wayBoundsConfig: RasterizeConfig(draw: true, color: nil, width: 2),
+                centroidConfig: RasterizeConfig(draw: true, color: nil, width: 5)
+            )!,
+            scale: 1.0, orientation: .up)
+        
+        return (annotatedDetectedObjects: annotatedDetectedObjects,
+                selectedObjectId: selectedObjectId,
+                uiImage: objectsUIImage)
+    }
+    
+    func updateObjectSelection(previousSelectedObjectId: UUID?, selectedObjectId: UUID) {
+        guard let baseImage = self.objectsUIImage?.cgImage else {
+            print("Base image is nil")
+            return
+        }
+        
+        var oldObjects: [DetectedObject] = []
+        var newObjects: [DetectedObject] = []
+        var newImage: CGImage?
+        
+        if let previousSelectedObjectId = previousSelectedObjectId {
+            for object in self.annotatedDetectedObjects ?? [] {
+                if object.id == previousSelectedObjectId {
+                    if object.object != nil { oldObjects.append(object.object!) }
+                    break
+                }
+            }
+        }
+        newImage = ContourObjectRasterizer.updateRasterizedImage(
+            baseImage: baseImage, objects: oldObjects, size: Constants.ClassConstants.inputSize,
+            polygonConfig: RasterizeConfig(draw: true, color: nil, width: 2),
+            boundsConfig: RasterizeConfig(draw: false, color: nil, width: 0),
+            wayBoundsConfig: RasterizeConfig(draw: true, color: nil, width: 2),
+            centroidConfig: RasterizeConfig(draw: true, color: nil, width: 5)
+        )
+        if newImage == nil { print("Failed to update rasterized image") }
+        
+        for object in self.annotatedDetectedObjects ?? [] {
+            if object.id == selectedObjectId {
+                if object.object != nil { newObjects.append(object.object!) }
+                break
+            }
+        }
+        newImage = newImage ?? baseImage
+        newImage = ContourObjectRasterizer.updateRasterizedImage(
+            baseImage: newImage!, objects: newObjects, size: Constants.ClassConstants.inputSize,
+            polygonConfig: RasterizeConfig(draw: true, color: .white, width: 2),
+            boundsConfig: RasterizeConfig(draw: false, color: nil, width: 0),
+            wayBoundsConfig: RasterizeConfig(draw: true, color: .white, width: 2),
+            centroidConfig: RasterizeConfig(draw: true, color: .white, width: 5)
+        )
+        if let newImage = newImage {
+            self.objectsUIImage = UIImage(cgImage: newImage, scale: 1.0, orientation: .up)
+        } else { print("Failed to update rasterized image") }
     }
 }
