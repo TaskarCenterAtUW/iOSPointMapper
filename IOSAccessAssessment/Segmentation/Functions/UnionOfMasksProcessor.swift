6d2996bad8b44322639cc3805bf71e30b6a5dde8
himanshunaidu <himanshunaidu12161997@gmail.com>
2025-06-29 22:20:23 -0700

    Back union of masks result by pixel buffer

--- IOSAccessAssessment/Segmentation/Functions/UnionOfMasksProcessor.swift
+++ IOSAccessAssessment/Segmentation/Functions/UnionOfMasksProcessor.swift
@@ -153,8 +153,14 @@ class UnionOfMasksProcessor {
         
         commandBuffer.commit()
         commandBuffer.waitUntilCompleted()
-
-        return CIImage(mtlTexture: outputTexture, options: [.colorSpace: CGColorSpaceCreateDeviceGray()])
+        
+        let image = CIImage(mtlTexture: outputTexture, options: [.colorSpace: CGColorSpaceCreateDeviceGray()])
+        guard let image = image else {
+            print("Error: Failed to create CIImage from output texture")
+            return nil
+        }
+        // Create a CIImage from the outputTexture backed by a CVPixelBuffer, because it will be mostly accessed by CPU
+        return self.backCIImageToPixelBuffer(image)
     }
     
     private func ciImageToTexture(image: CIImage, descriptor: MTLTextureDescriptor, options: [MTKTextureLoader.Option: Any]) -> MTLTexture? {
@@ -177,6 +183,32 @@ class UnionOfMasksProcessor {
         default:
             fatalError("Unsupported pixel format: \(format.rawValue)")
         }
+    }
+    
+    private func backCIImageToPixelBuffer(_ image: CIImage) -> CIImage {
+        var imageBuffer: CVPixelBuffer?
+        let attributes: [CFString: Any] = [
+            kCVPixelBufferCGImageCompatibilityKey: true,
+            kCVPixelBufferCGBitmapContextCompatibilityKey: true,
+            kCVPixelBufferIOSurfacePropertiesKey: [:] // Required for Metal/CoreImage
+        ]
+        let status = CVPixelBufferCreate(
+            kCFAllocatorDefault,
+            self.width,
+            self.height,
+            kCVPixelFormatType_OneComponent8,
+            attributes as CFDictionary,
+            &imageBuffer
+        )
+        guard status == kCVReturnSuccess, let imageBuffer = imageBuffer else {
+            print("Error: Failed to create pixel buffer")
+            return image
+        }
+        // Render the CIImage to the pixel buffer
+        self.ciContext.render(image, to: imageBuffer, bounds: image.extent, colorSpace: CGColorSpaceCreateDeviceGray())
+        // Create a CIImage from the pixel buffer
+        let ciImage = CIImage(cvPixelBuffer: imageBuffer)
+        return ciImage
     }
 
 }
