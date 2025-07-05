5f154f62b415cb26c531b13a2094a19808d4aea4
himanshunaidu <himanshunaidu12161997@gmail.com>
2025-07-04 18:45:22 -0700

    Add encoders for video and depth frames

--- IOSAccessAssessment/Dataset/DatasetEncoder.swift
+++ IOSAccessAssessment/Dataset/DatasetEncoder.swift
@@ -17,9 +17,50 @@ enum DatasetEncoderStatus {
 }
 
 class DatasetEncoder {
-    private let datasetDirectory: URL
+    private var datasetDirectory: URL
+    private var currentFrame: Int = -1
+    private var savedFrames: Int = 0
     
-    init() {
+    public let rgbFilePath: URL // Relative to app document directory.
+    public let depthFilePath: URL // Relative to app document directory.
+    public let confidenceFilePath: URL // Relative to app document directory.
+    public let cameraMatrixPath: URL
+    public let cameraTransformPath: URL
+    public let locationPath: URL
+    public let headingPath: URL
+    
+    private let rgbEncoder: RGBEncoder
+    private let depthEncoder: DepthEncoder
+    
+    public var status = DatasetEncoderStatus.allGood
+    
+    init(changesetId: String) {
         self.datasetDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        datasetDirectory = DatasetEncoder.createDirectory(id: changesetId)
+        self.rgbFilePath = datasetDirectory.appendingPathComponent("rgb", isDirectory: true)
+        self.depthFilePath = datasetDirectory.appendingPathComponent("depth", isDirectory: true)
+        self.confidenceFilePath = datasetDirectory.appendingPathComponent("confidence", isDirectory: true)
+        self.cameraMatrixPath = datasetDirectory.appendingPathComponent("camera_matrix.csv", isDirectory: false)
+        self.cameraTransformPath = datasetDirectory.appendingPathComponent("camera_transform.csv", isDirectory: false)
+        self.locationPath = datasetDirectory.appendingPathComponent("location.csv", isDirectory: false)
+        self.headingPath = datasetDirectory.appendingPathComponent("heading.csv", isDirectory: false)
+        
+        self.rgbEncoder = RGBEncoder(outDirectory: self.rgbFilePath)
+        self.depthEncoder = DepthEncoder(outDirectory: self.depthFilePath)
+    }
+    
+    static private func createDirectory(id: String) -> URL {
+        let url = FileManager.default.urls(for:.documentDirectory, in: .userDomainMask).first!
+        var directory = URL(filePath: id, directoryHint: .isDirectory, relativeTo: url)
+        if FileManager.default.fileExists(atPath: directory.path) {
+            // Return existing directory if it already exists
+            return directory
+        }
+        do {
+            try FileManager.default.createDirectory(at: directory, withIntermediateDirectories: true, attributes: nil)
+        } catch let error as NSError {
+            print("Error creating directory. \(error), \(error.userInfo)")
+        }
+        return directory
     }
 }
