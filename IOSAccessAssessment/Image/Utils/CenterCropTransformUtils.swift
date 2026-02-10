23e006271f878a7848a5ec33ce62cb93faae5b29
himanshunaidu <himanshunaidu12161997@gmail.com>
2026-02-09 19:51:50 -0800

    Implement the revert center crop on Metal

--- IOSAccessAssessment/Image/Utils/CenterCropTransformUtils.swift
+++ IOSAccessAssessment/Image/Utils/CenterCropTransformUtils.swift
@@ -50,7 +50,7 @@ struct CenterCropTransformUtils {
         self.ciContext = CIContext(mtlDevice: device, options: [.workingColorSpace: NSNull(), .outputColorSpace: NSNull()])
     }
     
-    func revertCenterCropAspectFit(_ image: CIImage, to destSize: CGSize) -> CIImage {
+    func revertCenterCropAspectFit(_ image: CIImage, to destSize: CGSize) throws -> CIImage {
         let sourceSize = image.extent.size
         let sourceAspect = sourceSize.width / sourceSize.height
         let destAspect = destSize.width / destSize.height
@@ -67,6 +67,50 @@ struct CenterCropTransformUtils {
             let scaledHeight = sourceSize.height * scale
             offsetY = (destSize.height - scaledHeight) / 2
         }
-        return image
+        var params = RevertCenterCropParams(
+            srcWidth: UInt32(sourceSize.width),
+            srcHeight: UInt32(sourceSize.height),
+            dstWidth: UInt32(destSize.width),
+            dstHeight: UInt32(destSize.height),
+            scale: Float(scale),
+            offset: SIMD2<Float>(Float(offsetX), Float(offsetY))
+        )
+        
+        let descriptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: .r8Unorm, width: Int(destSize.width), height: Int(destSize.height), mipmapped: false)
+        descriptor.usage = [.shaderRead, .shaderWrite]
+        guard let commandBuffer = self.commandQueue.makeCommandBuffer() else {
+            throw CenterCropTransformUtilsError.metalPipelineCreationError
+        }
+        let sourceTexture: MTLTexture = try image.toMTLTexture(
+            device: device, commandBuffer: commandBuffer, pixelFormat: .r8Unorm,
+            context: ciContext, colorSpace: CGColorSpaceCreateDeviceRGB()
+        )
+        guard let destTexture = self.device.makeTexture(descriptor: descriptor) else {
+            throw CenterCropTransformUtilsError.textureCreationFailed
+        }
+        guard let kernelFunction = device.makeDefaultLibrary()?.makeFunction(name: "revertCenterCropAspectFitKernel"),
+              let pipeline = try? device.makeComputePipelineState(function: kernelFunction) else {
+            throw CenterCropTransformUtilsError.metalInitializationFailed
+        }
+        guard let commandEncoder = commandBuffer.makeComputeCommandEncoder() else {
+            throw CenterCropTransformUtilsError.metalPipelineCreationError
+        }
+        commandEncoder.setComputePipelineState(pipeline)
+        commandEncoder.setTexture(sourceTexture, index: 0)
+        commandEncoder.setTexture(destTexture, index: 1)
+        commandEncoder.setBytes(&params, length: MemoryLayout<RevertCenterCropParams>.stride, index: 0)
+        let threadgroupSize = MTLSize(width: pipeline.threadExecutionWidth, height: pipeline.maxTotalThreadsPerThreadgroup / pipeline.threadExecutionWidth, depth: 1)
+        let threadgroups = MTLSize(width: (Int(destSize.width) + threadgroupSize.width - 1) / threadgroupSize.width,
+                                   height: (Int(destSize.height) + threadgroupSize.height - 1) / threadgroupSize.height,
+                                   depth: 1)
+        commandEncoder.dispatchThreadgroups(threadgroups, threadsPerThreadgroup: threadgroupSize)
+        commandEncoder.endEncoding()
+        commandBuffer.commit()
+        commandBuffer.waitUntilCompleted()
+        
+        guard let destCIImage = CIImage(mtlTexture: destTexture, options: [.colorSpace: NSNull()]) else {
+            throw CenterCropTransformUtilsError.outputImageCreationFailed
+        }
+        return destCIImage
     }
 }
