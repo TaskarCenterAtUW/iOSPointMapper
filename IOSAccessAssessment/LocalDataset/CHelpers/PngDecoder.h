1ceaddb846945a44985f8c80fb8cf287dd4bd727
himanshunaidu <himanshunaidu12161997@gmail.com>
2026-03-02 16:05:21 -0800

    Add initial version of Png Decoder

--- IOSAccessAssessment/LocalDataset/CHelpers/PngDecoder.h
+++ IOSAccessAssessment/LocalDataset/CHelpers/PngDecoder.h
@@ -8,8 +8,13 @@
 #ifndef PngDecoder_h
 #define PngDecoder_h
 #include <Foundation/Foundation.h>
-
+NS_ASSUME_NONNULL_BEGIN
 @interface PngDecoder : NSObject
 
+- (instancetype) initWithContentsOfFile:(NSData*)fileContents;
+- (NSData * _Nullable)depthDataWithWidth:(int *)width
+                                  height:(int *)height;
+
 @end
+NS_ASSUME_NONNULL_END
 #endif /* PngDecoder_h */
--- IOSAccessAssessment/LocalDataset/CHelpers/PngDecoder.mm
+++ IOSAccessAssessment/LocalDataset/CHelpers/PngDecoder.mm
@@ -9,3 +9,57 @@
 #import "PngDecoder.h"
 #import "lodepng.h"
 #include <cmath>
+
+@implementation PngDecoder {
+    NSData *_fileData;
+}
+- (instancetype)initWithContentsOfFile:(NSData *)fileContents {
+    self = [super init];
+    if (self) {
+        _fileData = fileContents;
+    }
+    return self;
+}
+- (NSData *)depthDataWithWidth:(int *)width height:(int *)height {
+    std::vector<unsigned char> pngData(
+        (unsigned char *)_fileData.bytes,
+        (unsigned char *)_fileData.bytes + _fileData.length
+    );
+
+    std::vector<unsigned char> image;
+    unsigned w = 0;
+    unsigned h = 0;
+
+    unsigned error = lodepng::decode(
+        image,
+        w,
+        h,
+        pngData,
+        LCT_GREY,
+        16
+    );
+
+    if (error) {
+        return nil;
+    }
+
+    size_t pixelCount = w * h;
+
+    std::vector<float> floatDepth(pixelCount);
+
+    for (size_t i = 0; i < pixelCount; i++) {
+        uint16_t msb = image[i * 2];
+        uint16_t lsb = image[i * 2 + 1];
+
+        uint16_t depthMM = (msb << 8) | lsb;
+
+        floatDepth[i] = depthMM / 1000.0f; // mm → meters
+    }
+
+    *width = (int)w;
+    *height = (int)h;
+
+    return [NSData dataWithBytes:floatDepth.data()
+                          length:pixelCount * sizeof(float)];
+}
+@end
