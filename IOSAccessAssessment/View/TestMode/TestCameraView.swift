38ceff997493d718d5f752a7eab1b058cbc685dc
himanshunaidu <himanshunaidu12161997@gmail.com>
2026-03-05 19:20:27 -0800

    Bring the test camera view more in line with ar camera view

--- IOSAccessAssessment/View/TestMode/TestCameraView.swift
+++ IOSAccessAssessment/View/TestMode/TestCameraView.swift
@@ -8,6 +8,28 @@
 import SwiftUI
 import CoreLocation
 
+/**
+ Additional constants unique to TestCameraView (not used in ARCameraView)
+ */
+enum TestCameraViewConstants {
+    enum Texts {
+        static let contentViewTitle = "Test: Capture"
+        
+        /// Camera Hint Texts
+        static let cameraHintPlaceholderText = "..."
+        
+        /// ARCameraLearnMoreSheetView
+        static let testCameraLearnMoreSheetTitle = "About Capture"
+        static let testCameraLearnMoreSheetMessage = """
+        Use this screen to simulate capturing of accessibility features in your environment using local data. 
+        
+        Select the desired image, and press the Camera Button to take a snapshot.
+        
+        After capturing, you will be prompted to validate the annotated features.
+        """
+    }
+}
+
 enum TestCameraViewError: Error, LocalizedError {
     case captureDataUnavailable
     case captureNoSegmentationAccessibilityFeatures
@@ -19,16 +41,6 @@ enum TestCameraViewError: Error, LocalizedError {
         case .captureNoSegmentationAccessibilityFeatures:
             return "No accessibility features were captured. Please try again."
         }
-    }
-}
-
-class TestCameraManagerStatusViewModel: ObservableObject {
-    @Published var isFailed: Bool = false
-    @Published var errorMessage: String = ""
-    
-    func update(isFailed: Bool, errorMessage: String) {
-        self.isFailed = isFailed
-        self.errorMessage = errorMessage
     }
 }
 
@@ -47,14 +59,19 @@ struct TestCameraView: View {
     @Environment(\.dismiss) var dismiss
     
     @StateObject private var manager: TestCameraManager = TestCameraManager()
-    @StateObject private var managerConfigureStatusViewModel = TestCameraManagerStatusViewModel()
+    @StateObject private var managerConfigureStatusViewModel = ARCameraManagerStatusViewModel()
+    @State private var cameraHintText: String = ARCameraViewConstants.Texts.cameraHintPlaceholderText
     
-    @State private var currentIndex: Int = 0
+//    var locationManager: LocationManager = LocationManager()
+//    @State private var captureLocation: CLLocationCoordinate2D?
+    
+    @State private var showARCameraLearnMoreSheet = false
     
     @State private var showAnnotationView = false
     
     // Latest dataset capture data
     @State private var datasetCaptureData: DatasetCaptureData?
+    @State private var currentIndex: Int = 0
     
     var body: some View {
         VStack {
@@ -63,7 +80,7 @@ struct TestCameraView: View {
                     HostedTestCameraViewContainer(arSessionCameraProcessingDelegate: manager)
                     VStack {
                         /// Text for hinting user with status
-                        Text("Processing")
+                        Text(cameraHintText)
                             .padding()
                             .background(.regularMaterial, in: RoundedRectangle(cornerRadius: 8))
                             .frame(maxWidth: 300)
@@ -82,20 +99,20 @@ struct TestCameraView: View {
                             .padding(.bottom, 20)
                             Spacer()
                         }
-//                        .overlay(
-//                            reverseOrientationStack {
-//                                Spacer()
-//                                Button(action: {
-//                                    showARCameraLearnMoreSheet = true
-//                                }) {
-//                                    Image(systemName: ARCameraViewConstants.Images.infoIcon)
-//                                        .resizable()
-//                                        .frame(width: 20, height: 20)
-//                                }
-//                                .padding(.horizontal, 20)
-//                                .padding(.bottom, 20)
-//                            }
-//                        )
+                        .overlay(
+                            reverseOrientationStack {
+                                Spacer()
+                                Button(action: {
+                                    showARCameraLearnMoreSheet = true
+                                }) {
+                                    Image(systemName: ARCameraViewConstants.Images.infoIcon)
+                                        .resizable()
+                                        .frame(width: 20, height: 20)
+                                }
+                                .padding(.horizontal, 20)
+                                .padding(.bottom, 20)
+                            }
+                        )
                     }
                 }
             }
@@ -103,10 +120,69 @@ struct TestCameraView: View {
                 ProgressView(ARCameraViewConstants.Texts.cameraInProgressText)
             }
         }
-        .navigationBarTitle("Test Mapping", displayMode: .inline)
+        .navigationBarTitle(TestCameraViewConstants.Texts.contentViewTitle, displayMode: .inline)
         .onAppear {
-            onAppearLoadData()
+            showAnnotationView = false
+            segmentationPipeline.setSelectedClasses(selectedClasses)
+            do {
+                let datasetDecoder = try initializeDatasetDecoder()
+                let datasetCaptureData = try loadData(datasetDecoder: datasetDecoder)
+                sharedAppData.currentDatasetDecoder = datasetDecoder
+                self.datasetCaptureData = datasetCaptureData
+                try manager.configure(
+                    selectedClasses: selectedClasses, segmentationPipeline: segmentationPipeline,
+                    metalContext: sharedAppContext.metalContext,
+                    cameraOutputImageCallback: cameraOutputImageCallback
+                )
+                try manager.handleSessionFrameUpdate(datasetCaptureData: datasetCaptureData)
+            } catch {
+                managerConfigureStatusViewModel.update(isFailed: true, errorMessage: error.localizedDescription)
+            }
         }
+        .onDisappear {
+        }
+        .alert(ARCameraViewConstants.Texts.managerStatusAlertTitleKey, isPresented: $managerConfigureStatusViewModel.isFailed, actions: {
+            Button(ARCameraViewConstants.Texts.managerStatusAlertDismissButtonKey) {
+                managerConfigureStatusViewModel.update(isFailed: false, errorMessage: "")
+                dismiss()
+            }
+        }, message: {
+            Text(managerConfigureStatusViewModel.errorMessage)
+        })
+//        .fullScreenCover(isPresented: $showAnnotationView) {
+//            if let captureLocation {
+//                AnnotationView(selectedClasses: selectedClasses, captureLocation: captureLocation)
+//            } else {
+//                InvalidContentView(
+//                    title: ARCameraViewConstants.Texts.invalidContentViewTitle,
+//                    message: ARCameraViewConstants.Texts.invalidContentViewMessage
+//                )
+//            }
+//        }
+        .onChange(of: showAnnotationView, initial: false) { oldValue, newValue in
+            // If the AnnotationView is dismissed, reconfigure the manager for a new session
+//            if (oldValue == true && newValue == false) {
+//                do {
+////                    captureLocation = nil
+////                    try manager.resume()
+//                } catch {
+//                    managerConfigureStatusViewModel.update(isFailed: true, errorMessage: error.localizedDescription)
+//                }
+//            }
+        }
+        .sheet(isPresented: $showARCameraLearnMoreSheet) {
+            ARCameraLearnMoreSheetView()
+                .presentationDetents([.medium, .large])
+        }
+    }
+    
+    private func initializeDatasetDecoder() throws -> DatasetDecoder {
+        return try DatasetDecoder(workspaceId: workspaceId, changesetId: changesetId)
+    }
+    
+    private func loadData(datasetDecoder: DatasetDecoder) throws -> DatasetCaptureData {
+        let datasetCaptureData = try datasetDecoder.loadData(index: currentIndex)
+        return datasetCaptureData
     }
     
     @ViewBuilder
@@ -121,35 +197,6 @@ struct TestCameraView: View {
         manager.interfaceOrientation.isLandscape ?
         AnyLayout(VStackLayout())(content) :
         AnyLayout(HStackLayout())(content)
-    }
-    
-    private func onAppearLoadData() {
-        showAnnotationView = false
-        segmentationPipeline.setSelectedClasses(selectedClasses)
-        do {
-            let datasetDecoder = try initializeDatasetDecoder()
-            try manager.configure(
-                selectedClasses: selectedClasses, segmentationPipeline: segmentationPipeline,
-                metalContext: sharedAppContext.metalContext,
-                cameraOutputImageCallback: cameraOutputImageCallback
-            )
-            let datasetCaptureData = try loadData(datasetDecoder: datasetDecoder)
-            try manager.handleSessionFrameUpdate(datasetCaptureData: datasetCaptureData)
-            sharedAppData.currentDatasetDecoder = datasetDecoder
-            self.datasetCaptureData = datasetCaptureData
-        } catch {
-            print("Error configuring TestCameraManager: \(error)")
-            
-        }
-    }
-    
-    private func initializeDatasetDecoder() throws -> DatasetDecoder {
-        return try DatasetDecoder(workspaceId: workspaceId, changesetId: changesetId)
-    }
-    
-    private func loadData(datasetDecoder: DatasetDecoder) throws -> DatasetCaptureData {
-        let datasetCaptureData = try datasetDecoder.loadData(index: currentIndex)
-        return datasetCaptureData
     }
     
     private func cameraOutputImageCallback(_ captureImageData: (any CaptureImageDataProtocol)) {
@@ -186,8 +233,17 @@ struct TestCameraView: View {
                     captureImageData: captureData.imageData, captureMeshData: captureData.meshData, location: captureLocation
                 )
                 showAnnotationView = true
+            } catch ARCameraManagerError.finalSessionMeshUnavailable {
+                setHintText(ARCameraViewConstants.Texts.cameraHintNoMeshText)
+            } catch ARCameraManagerError.finalSessionNoSegmentationClass,
+                ARCameraViewError.captureNoSegmentationAccessibilityFeatures {
+                setHintText(ARCameraViewConstants.Texts.cameraHintNoSegmentationText)
+            } catch ARCameraManagerError.finalSessionNoSegmentationMesh {
+                setHintText(ARCameraViewConstants.Texts.cameraHintMeshNotProcessedText)
+            } catch _ as LocationManagerError {
+                setHintText(ARCameraViewConstants.Texts.cameraHintLocationErrorText)
             } catch {
-                print("Error during camera capture: \(error)")
+                setHintText(ARCameraViewConstants.Texts.cameraHintUnknownErrorText)
             }
         }
     }
@@ -207,6 +263,15 @@ struct TestCameraView: View {
             } catch {
                 print("Error adding capture data to dataset encoder: \(error)")
             }
+        }
+    }
+    
+    /// Set text for 2 seconds, and then fall back to placeholder
+    private func setHintText(_ text: String) {
+        cameraHintText = text
+        Task {
+            try await Task.sleep(for: .seconds(2))
+            cameraHintText = ARCameraViewConstants.Texts.cameraHintPlaceholderText
         }
     }
 }
